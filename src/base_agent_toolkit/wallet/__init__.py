33111f56459dec9b5aa9823c744361506ccb8025
nbraham <116988191+nbraham@users.noreply.github.com>
2026-06-23 12:30:45 +0700

    refactor(wallet): consolidate wallet module exports

--- src/base_agent_toolkit/wallet/__init__.py
+++ src/base_agent_toolkit/wallet/__init__.py
@@ -1,5 +1,24 @@
 """Wallet module for Base chain interactions."""
 
+from .contract import ContractWrapper
+from .erc20 import ERC20Token
+from .gas import GasOracle, GasPriceInfo
+from .hd import derive_wallets, generate_mnemonic
+from .simulator import SimulationResult, simulate_transaction
+from .tokens import TokenOperationsMixin
+from .tx_builder import TransactionBuilder
 from .wallet import BaseWallet
 
-__all__ = ["BaseWallet"]
+__all__ = [
+    "BaseWallet",
+    "ContractWrapper",
+    "ERC20Token",
+    "GasOracle",
+    "GasPriceInfo",
+    "SimulationResult",
+    "TokenOperationsMixin",
+    "TransactionBuilder",
+    "derive_wallets",
+    "generate_mnemonic",
+    "simulate_transaction",
+]
